7af03a0a6865344bdb5a548381352045d16b3ba1
Dirk Rathje <post@dirk-rathje.de>
2024-10-03 12:28:44 +0200

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
-const DEV_PORT = 2121;
+const DEV_PORT = 4321;
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,16 +11,7 @@ export default defineConfig({
 		? 'https://befide.github.io'
 		: `http://localhost:${DEV_PORT}`,
 	base: process.env.CI ? '/bf2035-data-dashboard' : undefined,
-
-	// output: 'server',
-
-	/* Like Vercel, Netlify,… Mimicking for dev. server */
-	// trailingSlash: 'always',
-
-	server: {
-		/* Dev. server only */
-		port: DEV_PORT,
-	},
+	trailingSlash: "always",
 
 	integrations: [
 		//
