0698cce6a5bef36f743b09d57d0ef531ba05e58a
Dirk Rathje <post@dirk-rathje.de>
2024-10-03 11:48:27 +0200

    fix github actions depolyment

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ const DEV_PORT = 2121;
 // https://astro.build/config
 export default defineConfig({
 	site: process.env.CI
-		? 'https://themesberg.github.io'
+		? 'https://befide.github.io'
 		: `http://localhost:${DEV_PORT}`,
 	base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
 
